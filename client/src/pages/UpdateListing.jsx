ed3796e3537997b51df28e6122083c2278a32d5f
Sravan <sravanssankar2000@gmail.com>
2025-03-13 04:52:56 +0530

    complete listing page

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -158,12 +158,16 @@ export default function CreateListing() {
             <div className='flex items-center gap-2'><input type='number' id='bathrooms' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.bathrooms} /><p>Baths</p></div>
             <div className='flex items-center gap-2'>
               <input type='number' id='regularPrice' min='50' max='10000000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.regularPrice} />
-              <div className='flex flex-col items-center'><p>Regular price</p><span className='text-xs'>($ / month)</span></div>
+              <div className='flex flex-col items-center'><p>Regular price</p>
+              {formData.type === 'rent' && (<span className='text-xs'>($ / month)</span>)}
+            </div>
             </div>
             {formData.offer && (
               <div className='flex items-center gap-2'>
                 <input type='number' id='discountPrice' min='0' max='10000000' required className='p-3 border border-gray-300 rounded-lg' onChange={handleChange} value={formData.discountPrice} />
-                <div className='flex flex-col items-center'><p>Discounted price</p><span className='text-xs'>($ / month)</span></div>
+                <div className='flex flex-col items-center'><p>Discounted price</p>
+                {formData.type === 'rent' && (<span className='text-xs'>($ / month)</span>)}
+                </div>
               </div>
             )}
           </div>
