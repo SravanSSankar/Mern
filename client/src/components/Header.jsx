a02e9d63a6771beff342c22042e714089f9e07af
Sravan <sravanssankar2000@gmail.com>
2025-03-17 01:03:41 +0530

    Minor changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,8 +26,8 @@ useEffect(() => {
        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Sravan</span>
-            <span className='text-slate-700'>Estate</span>
+            <span className='text-slate-500'>Sravan's</span>
+            <span className='text-slate-700'>Homysphere</span>
         </h1>
         </Link>
         <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -57,13 +57,13 @@ useEffect(() => {
     {/* top */}
     <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
       <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-        Find your next <span className='text-slate-500'>perfect</span>
+        Discover Your <span className='text-slate-500'>Dream Home</span>
         
         <br />
-        place with ease
+        Effortlessly
       </h1>
       <div className='text-gray-400 text-xs sm:text-sm'>
-        Sahand Estate is the best place to find your next perfect place to live.
+      HomySphere is the best place to find your next perfect place to live.
         <br />
         We have a wide range of properties for you to choose from.
       </div>
